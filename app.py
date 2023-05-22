de4452479c2442eadcac3f05e9b74b5d7c04a192
BeshoyMarkElMallah <bisho.king.980@gmail.com>
2023-05-22 15:18:55 +0300

    add plotting and writing to xlsx files

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 import random
 import math
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from dataclasses import dataclass
+import time
+
+
+
+
+# data = {
+#     "cust":[],
+#     "iat":[],
+#     "st":[],
+#     "arrival":[],
+#     "sstart":[],
+#     "send":[],
+#     "waiting":[],
+#     "qlen":[],
+#     "idle":[]
+# }
+
 
 # Set the value of lambda as an input (rate parameter)
 
@@ -19,8 +39,6 @@ def generate_Poisson_number(lam):
 
 
 # Import necessary modules
-import random
-from dataclasses import dataclass
 
 # Create a data class for rows that will hold simulation data
 @dataclass
@@ -42,16 +60,28 @@ num_of_cust_waiting=0
 service_time=0
 time_between_arrivals=0
 waiting_time_in_quene=0
+qlen_arr=[]
 
 grand_avg_waiting = 0  # average waiting time across all runs
 grand_max_qlen = 0  # maximum queue length across all runs
+avg_wait_arr=[]
 
 LAMDAIAT = 5  # inter-arrival rate
 LAMDAST = 0.33 # service rate
 
-# Run simulation NUM_OF_RUNS times
-for i in range(NUM_OF_RUNS):
-
+    # Run simulation NUM_OF_RUNS times
+for j in range(NUM_OF_RUNS):
+    data = {
+    "cust":[],
+    "iat":[],
+    "st":[],
+    "arrival":[],
+    "sstart":[],
+    "send":[],
+    "waiting":[],
+    "qlen":[],
+    "idle":[]
+    }
     avg_waiting = 0  # initialize average waiting time for this run
     max_qlen = 0  # initialize maximum queue length for this run
     server_idle = 0  # initialize amount of time server is idle for this run
@@ -65,7 +95,17 @@ for i in range(NUM_OF_RUNS):
     c1.idle = c1.iat  # server is idle until first customer arrives
     server_idle += c1.idle  # add idle time for this customer to total
     sim_table.append(c1)  # add customer object to simulation table at index 0
-
+    
+    # add customer object data to data dictionary to save to the xlsx file
+    data["cust"].append(f"C{1}")
+    data["iat"].append(c1.iat)
+    data["st"].append(c1.st)
+    data["arrival"].append(c1.arrival)
+    data["sstart"].append(c1.sstart)
+    data["send"].append(c1.send)
+    data["waiting"].append(c1.waiting)
+    data["qlen"].append(c1.qlen)
+    data["idle"].append(c1.idle)
     # Create remaining customer objects and calculate their simulation data
     for i in range(1,NUM_OF_CUSTOMERS):
         c = Row()  # create new customer object
@@ -101,26 +141,40 @@ for i in range(NUM_OF_RUNS):
         sim_run_time=c.send
         service_time+=c.st
         time_between_arrivals +=c.iat
-
+        
+        # add customer object data to data dictionary to save to the xlsx file
+        data["cust"].append(f"C{i+1}")
+        data["iat"].append(c.iat)
+        data["st"].append(c.st)
+        data["arrival"].append(c.arrival)
+        data["sstart"].append(c.sstart)
+        data["send"].append(c.send)
+        data["waiting"].append(c.waiting)
+        data["qlen"].append(c.qlen)
+        data["idle"].append(c.idle)
         sim_table.append(c)  # add customer object to simulation table
+        
+        
+    df  = pd.DataFrame(data)  # create dataframe from data dictionary
+    with pd.ExcelWriter(f'simulation_data.xlsx',mode='a',engine='openpyxl' ) as writer:    # create excel writer object
+        df.to_excel(writer, sheet_name=f"sheet{j+1}",index=False)  # write dataframe to excel sheet
+    
+    
+
 
     avg_waiting /= NUM_OF_CUSTOMERS  # calculate average waiting time for this run
-   # server_idle /= sim_table[NUM_OF_CUSTOMERS-1].send  # calculate server idle time for this run
+    avg_wait_arr.append(avg_waiting)
+    # server_idle /= sim_table[NUM_OF_CUSTOMERS-1].send  # calculate server idle time for this run
 
     grand_avg_waiting += avg_waiting  # add average waiting time for this run to total for all runs
-    grand_max_qlen = max(max_qlen,grand_max_qlen)  # update max queue length across all runs if necessary
-
-    # calculate overall average waiting time across all runs
-
-    # Print out simulation data table
+    grand_max_qlen = max(max_qlen,grand_max_qlen)  # update max queue length across all runs if necessary    # calculate overall average waiting time across all runs    # Print out simulation data table
+    qlen_arr.append(max_qlen)
     prob_that_cust_has_wait=num_of_cust_waiting/NUM_OF_CUSTOMERS #probability that customer has to wait 
     prob_of_server_idleness=server_idle/sim_run_time #proportion of server idleness
     avg_service_time = service_time/NUM_OF_CUSTOMERS #average srevice time
     avg_time_bet_arrivals= time_between_arrivals / (NUM_OF_CUSTOMERS-1) #average time between arrivals
     avg_waiting_time_who_wait= waiting_time_in_quene/num_of_cust_waiting #average waiting time of those who wait
-    avg_cust_spend_in_system = avg_waiting +avg_service_time #average time a customer spends in system
-
-    print("cust\t\tiat\t\tst\t\tarrival\t\tsstart\t\tsend\t\twaiting\t\tqlen\t\tidle")
+    avg_cust_spend_in_system = avg_waiting +avg_service_time #average time a customer spends in system    print("cust\t\tiat\t\tst\t\tarrival\t\tsstart\t\tsend\t\twaiting\t\tqlen\t\tidle")
     for i in range(NUM_OF_CUSTOMERS):
         print("C"+str(i+1),'\t\t',sim_table[i].iat,'\t\t',sim_table[i].st,'\t\t',sim_table[i].arrival,'\t\t',sim_table[i].sstart,'\t\t',sim_table[i].send,'\t\t',sim_table[i].waiting,'\t\t',sim_table[i].qlen,'\t\t',sim_table[i].idle)
     print('avg_waiting:= ',avg_waiting)
@@ -135,9 +189,24 @@ for i in range(NUM_OF_RUNS):
     print('\n\n')
     sim_table.clear()  # clear simulation table for next run
     
-
+    
+    
 grand_avg_waiting /= NUM_OF_RUNS    # calculate overall average waiting time across all runs
 
 print('grand_avg_waiting:= ',grand_avg_waiting) # print overall average waiting time across all runs
 print('grand_max_qlen:= ',grand_max_qlen) # print max queue length across all runs
+
+#plotting average waiting time graph
+plt.title("Average Waiting Time Graph") 
+plt.xlabel("Run")  
+plt.ylabel("Average Waiting Time")  
+plt.plot(avg_wait_arr) 
+plt.show() 
+
+#plotting queue length graph
+plt.title("Queue Length Graph")
+plt.xlabel("Run")
+plt.ylabel("Queue Length")
+plt.plot(qlen_arr)
+plt.show()
 
